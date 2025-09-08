ef4a4a689aaeb5d25df1daab22aa0f08d48d4e0d
shekofaamiri <shekofaamiri1@gmail.com>
2025-09-08 10:11:23 +0330

    elif condition added.

--- triangle.cpp
+++ triangle.cpp
@@ -3,3 +3,5 @@ b = float(input("Enter the second side: "))
 c = float(input("Enter the third side: "))
 if a <= 0 or b <= 0 or c <= 0:
     print("Sides must be positive numbers.")
+    elif a + b > c and a + c > b and b + c > a:
+    print("Yes, it is a triangle.")
