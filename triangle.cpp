760300074addcc2ff5d62b2915fc982dadd61a65
shekofaamiri <shekofaamiri1@gmail.com>
2025-09-08 10:10:12 +0330

    if condition added.

--- triangle.cpp
+++ triangle.cpp
@@ -1,3 +1,5 @@
 a = float(input("Enter the first side: "))
 b = float(input("Enter the second side: "))
 c = float(input("Enter the third side: "))
+if a <= 0 or b <= 0 or c <= 0:
+    print("Sides must be positive numbers.")
