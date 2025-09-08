2b292e6926e056e359b4b9f9c53e1ade6e00151a
shekofaamiri <shekofaamiri1@gmail.com>
2025-09-08 10:12:39 +0330

    else condition added.

--- triangle.cpp
+++ triangle.cpp
@@ -5,3 +5,5 @@ if a <= 0 or b <= 0 or c <= 0:
     print("Sides must be positive numbers.")
     elif a + b > c and a + c > b and b + c > a:
     print("Yes, it is a triangle.")
+    else:
+    print("No, it is not a triangle.")
